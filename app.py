058968bbe80f60d57698b730e5224aa144910092
Christine Nguyen <christine_nguyenl@berkeley.edu>
2020-10-18 15:29:17 -0700

    added more to tab one

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import graphs
 app = dash.Dash(__name__)
 
 def build_banner():
+    """ Build the banner at the top of the page.
+    """
     return html.Div(
         id = "banner",
         className = "container-row",
@@ -33,24 +35,47 @@ def build_banner():
     )
 
 def build_tabs():
-    return html.Div([
-        dcc.Tabs(
-            id = 'tabs-example', 
-            value = 'tab-1', 
-            children = [
-                dcc.Tab(label = 'Select Weather File', value = 'tab-1'),
-                dcc.Tab(label = 'Climate Summary', value = 'tab-2'),
-                dcc.Tab(label = 'Temperature and Humidity', value = 'tab-3'),
-                dcc.Tab(label = 'Sun and Rain', value = 'tab-4'),
-                dcc.Tab(label = 'Wind', value = 'tab-5'),
-                dcc.Tab(label = 'Outdoor Comfort', value = 'tab-6'),
-                dcc.Tab(label = 'Natural Ventilation', value = 'tab-7'),
-            ]),
-        html.Div(id = 'tabs-example-content')
-    ])
+    """ Build the seven different tabs. 
+    """
+    return html.Div(
+        id = "tabs-container",
+        children = [
+            dcc.Tabs(
+                id = 'tabs', 
+                value = 'tab-1', 
+                children = [
+                    dcc.Tab(label = 'Select Weather File', value = 'tab-1'),
+                    dcc.Tab(label = 'Climate Summary', value = 'tab-2'),
+                    dcc.Tab(label = 'Temperature and Humidity', value = 'tab-3'),
+                    dcc.Tab(label = 'Sun and Rain', value = 'tab-4'),
+                    dcc.Tab(label = 'Wind', value = 'tab-5'),
+                    dcc.Tab(label = 'Outdoor Comfort', value = 'tab-6'),
+                    dcc.Tab(label = 'Natural Ventilation', value = 'tab-7'),
+                ]
+            ),
+            html.Div(id = 'tabs-content')
+        ]
+    )
+
+def tab_one():
+    return html.Div(
+        id = "tab-one-container",
+        className = "container-col",
+        children = [
+            html.Label('Copy paste a link from the map below'),
+            dcc.Input(
+                id = "input-url",
+                value = 'https://energyplus.net/weather-download/north_and_central_america_wmo_region_4/USA/CA/USA_CA_Oakland.Intl.AP.724930_TMY/USA_CA_Oakland.Intl.AP.724930_TMY.epw', 
+                type = 'text'
+            ),
+            html.Embed(
+                id = "tab-one-map",
+                src = "https://www.ladybug.tools/epwmap/"
+            )
+        ]
+    )
 
 def tab_two():
-    # return html.H2("Hello")
     return html.Div(
             children = [
                 html.Div(
@@ -83,16 +108,12 @@ def tab_two():
             ]
         )
 
-@app.callback(Output('tabs-example-content', 'children'),
-              [Input('tabs-example', 'value')])
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
 
 def render_content(tab):
     if tab == 'tab-1':
-        return html.Div(
-            children = [
-                html.H3('Tab content 1')
-            ]
-        )
+        return tab_one()
     elif tab == 'tab-2':
         return tab_two()
     elif tab == 'tab-3':
