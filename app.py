b69a1f8c207559efb7842c4db4e96f1fe7ba2b2b
Christine Nguyen <christine_nguyenl@berkeley.edu>
2020-10-18 12:45:10 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,117 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+from extract_df import create_df
+
+app = dash.Dash(__name__)
+
+url = "https://energyplus.net/weather-download/north_and_central_america_wmo_region_4/USA/CA/USA_CA_Oakland.Intl.AP.724930_TMY/USA_CA_Oakland.Intl.AP.724930_TMY.epw"
+epw_df, location_name = create_df(url)
+
+# Color scheme and templates
+DBT_color='Reds'
+RH_color='GnBu'
+GHrad_color='YlOrRd_r'
+Wspeed_color='Blues_r'
+template="ggplot2"
+
+# First violin plot 
+def create_violin():
+    custom_ylim = (-40, 50)
+    Title = "Temperature" + " profile<br>" + location_name
+    fig = px.violin(data_frame=epw_df, x=None, y="DBT", template=template, 
+        range_y=custom_ylim, height=1000, width=350, points=False, box=False, 
+        title=Title, labels=dict(DBT="Temperature (degC)"))
+    return fig
+
+def build_banner():
+    return html.Div(
+        id = "banner",
+        className = "container-row",
+        children = [
+            html.Div(
+                id = "banner-text-left",
+                className = "container-col",
+                children = [
+                    html.H1("EPW Viz Tool"),
+                    html.H4("Subtitle"),
+                ]
+            ),
+            html.Div(
+                id = "banner-text-right",
+                className = "container-col",
+                children = [
+                    html.H4("Some text"),
+                    html.H4("More text"),
+                ]
+            )
+        ]
+    )
+
+def build_tabs():
+    return html.Div([
+        dcc.Tabs(
+            id = 'tabs-example', 
+            value = 'tab-1', 
+            children = [
+                dcc.Tab(label = 'Select Weather File', value = 'tab-1'),
+                dcc.Tab(label = 'Climate Summary', value = 'tab-2'),
+                dcc.Tab(label = 'Temperature and Humidity', value = 'tab-3'),
+                dcc.Tab(label = 'Sun and Rain', value = 'tab-4'),
+                dcc.Tab(label = 'Wind', value = 'tab-5'),
+                dcc.Tab(label = 'Outdoor Comfort', value = 'tab-6'),
+                dcc.Tab(label = 'Natural Ventilation', value = 'tab-7'),
+            ]),
+        html.Div(id = 'tabs-example-content')
+    ])
+
+@app.callback(Output('tabs-example-content', 'children'),
+              [Input('tabs-example', 'value')])
+
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Tab content 1')
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
+    elif tab == 'tab-3':
+        return html.Div([
+            html.H3('Tab content 3')
+        ])
+    elif tab == 'tab-4':
+        return html.Div([
+            html.H3('Tab content 4')
+        ])
+    elif tab == 'tab-5':
+        return html.Div([
+            html.H3('Tab content 5')
+        ])
+    elif tab == 'tab-6':
+        return html.Div([
+            html.H3('Tab content 6')
+        ])
+    elif tab == 'tab-7':
+        return html.Div([
+            html.H3('Tab content 7')
+        ])
+
+app.layout = html.Div(
+    id='big-container',
+    children=[
+        build_banner(),
+        build_tabs(),
+        dcc.Graph(
+            id='example-graph',
+            figure=create_violin()
+        )
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ extract_df.py
@@ -0,0 +1,161 @@
+from urllib.request import Request, urlopen
+import pandas as pd
+
+def create_df(url):
+    """ Return the dataframe from the url and the location name. 
+    """
+    location_name = create_columns(get_data(url))
+    epw_df = pd.DataFrame(
+        {
+            "year":year,
+            "fake_year":fake_year,
+            "DOY":DOY,
+            "month":month,
+            "month_names_long":month_names_long,
+            "day":day,
+            "hour":hour,
+            "DBT":DBT,
+            "DPT":DPT,
+            "RH":RH,
+            "Apressure":Apressure,
+            "EHrad":EHrad,
+            "HIRrad":HIRrad,
+            "GHrad":GHrad,
+            "DNrad":DNrad,
+            "DifHrad":DifHrad,
+            "GHillum":GHillum,
+            "DNillum":DNillum,
+            "DifHillum":DifHillum,
+            "Zlumi":Zlumi,
+            "Wdir":Wdir,
+            "Wspeed":Wspeed,
+            "Tskycover":Tskycover,
+            "Oskycover":Oskycover,
+            "Vis":Vis,
+            "Cheight":Cheight,
+            "PWobs":PWobs,
+            "PWcodes":PWcodes,
+            "Pwater":Pwater,
+            "AsolOptD":AsolOptD,
+            "SnowD":SnowD,
+            "DaySSnow":DaySSnow
+        }
+    )
+    return epw_df, location_name
+
+def create_columns(lines):
+    """ Populate the column lists.
+    """
+    for i, line in enumerate(lines):
+        if i == 0: #read header and site info
+            line=line.strip()
+            line01=line.split(',')
+
+            #print (line01)
+
+            City = line01[1]
+            Country = line01[3]
+            latitude = float(line01[-4])
+            longitude = float(line01[-3])
+            time_zone = float(line01[-2])
+            site_elevation = line01[-1]
+
+            station = City + '_' + Country
+            latitude = latitude
+            longitude = longitude
+            time_zone = time_zone
+            site_elevation = site_elevation
+
+            location = 'Location: ' + City + '_' + Country + ', Latitude:' + str(latitude) + ', Longitude:' + str(longitude) + ', Time Zone:' + str(time_zone) + ', ' + site_elevation + 'm above sea level'
+
+        elif i > 7 and i - 7 <= 8760: # Read all the individual data columns in the weather file
+
+            line = line.strip()
+            epwdata = line.split(",")
+            
+            year.append(int(epwdata[0]))
+            day.append(int(epwdata[2]))
+            month.append(int(epwdata[1]))
+            hour.append(int(epwdata[3]))
+            DBT.append(float(epwdata[6]))
+            DPT.append(float(epwdata[7]))
+            RH.append(float(epwdata[8]))
+            Apressure.append(float(epwdata[9]))
+            EHrad.append(float(epwdata[10]))
+            EDNrad.append(float(epwdata[11]))
+            HIRrad.append(float(epwdata[12]))
+            GHrad.append(float(epwdata[13]))
+            DNrad.append(float(epwdata[14]))
+            DifHrad.append(float(epwdata[15]))
+            GHillum.append(float(epwdata[16]))
+            DNillum.append(float(epwdata[17]))
+            DifHillum.append(float(epwdata[18]))
+            Zlumi.append(float(epwdata[19]))
+            Wdir.append(float(epwdata[20]))
+            Wspeed.append(float(epwdata[21]))
+            Tskycover.append(float(epwdata[22]))
+            Oskycover.append(float(epwdata[23]))
+            Vis.append(float(epwdata[24]))
+            Cheight.append(float(epwdata[25]))
+            PWobs.append(float(epwdata[26]))
+            PWcodes.append(float(epwdata[27]))
+            Pwater.append(float(epwdata[28]))
+            AsolOptD.append(float(epwdata[29]))
+            SnowD.append(float(epwdata[30]))
+            DaySSnow.append(float(epwdata[31]))
+
+    month_names = ["Jan", "Feb","Mar","Apr","May","Jun", "Jul", "Aug","Sep","Oct","Nov","Dec"]
+    doy = 1
+    for i, h in enumerate(hour):
+        DOY.append(doy)
+        fake_year.append("year")
+        month_names_long.append(month_names[month[i]-1])
+        if h == 24:
+            doy += 1
+    location_name = (City + ", " + Country)
+    return location_name
+
+def get_data(url):
+    """ Return a list of the data from api call. 
+    """
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(url, headers=headers)
+    epw = urlopen(req).read().decode()
+    lines = epw.split("\n")
+    return lines
+
+year=[]
+month=[]
+day=[]
+hour=[]
+DBT=[]
+DPT=[]
+RH=[]
+Apressure=[]
+EHrad=[]
+EDNrad=[]
+HIRrad=[]
+GHrad=[]
+DNrad=[]
+DifHrad=[]
+GHillum=[]
+DNillum=[]
+DifHillum=[]
+Zlumi=[]
+Wdir=[]
+Wspeed=[]
+Tskycover=[]
+Oskycover=[]
+Vis=[]
+Cheight=[]
+PWobs=[]
+PWcodes=[]
+Pwater=[]
+AsolOptD=[]
+SnowD=[]
+DaySSnow=[]
+month_names_long = []
+DOY = []
+fake_year = []
+
+ 
