f4c253414737a3814da377f7122c186d920f134e
FedericoTartarini <federicotartarini@gmail.com>
2021-06-09 17:30:04 +0800

    ignore: removed unnecessary folders

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 import pandas as pd
 from dash import Dash
-import dash
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
