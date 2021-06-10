b9612a35b84a74e34a56da17d699feaf7d89d0d5
FedericoTartarini <federicotartarini@gmail.com>
2021-06-10 15:41:24 +0800

    feat: cached charts tab temperature and humidity and created a function per chart

--- app.py
+++ app.py
@@ -45,7 +45,6 @@ import warnings
 # todo remove ignore warnings
 warnings.filterwarnings("ignore")
 
-
 app = Dash(
     __name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
@@ -186,40 +185,97 @@ def update_tab_two(ts, global_local, df, meta):
 ####################################
 ### TAB THREE: TEMP AND HUMIDITY ###
 ####################################
+@app.callback(
+    Output("yearly-dbt", "figure"),
+    [Input("global-local-radio-input", "value")],
+    [State("df-store", "data")],
+)
+@code_timer
+@cache.memoize(timeout=TIMEOUT)
+def update_tab_three_db_yearly(global_local, df):
+    """Update the contents of tab three. Passing in general info (df, meta)."""
+    df = pd.read_json(df, orient="split")
+    # Yearly Graphs
+    dbt_yearly = yearly_profile(df, "DBT", global_local)
+    dbt_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
+
+    return dbt_yearly
 
 
 @app.callback(
-    Output("yearly-dbt", "figure"),
     Output("daily-dbt", "figure"),
-    Output("heatmap-dbt", "figure"),
-    Output("yearly-rh", "figure"),
-    Output("daily-rh", "figure"),
-    Output("heatmap-rh", "figure"),
-    [Input("df-store", "modified_timestamp")],
     [Input("global-local-radio-input", "value")],
     [State("df-store", "data")],
-    [State("meta-store", "data")],
 )
 @code_timer
-def update_tab_three(ts, global_local, df, meta):
+@cache.memoize(timeout=TIMEOUT)
+def update_tab_three_db_daily(global_local, df):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
-
-    # Yearly Graphs
-    dbt_yearly = yearly_profile(df, "DBT", global_local)
-    rh_yearly = yearly_profile(df, "RH", global_local)
-    dbt_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
-    rh_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
-
-    # Daily Profile Graphs
     dbt_daily = daily_profile(df, "DBT", global_local)
-    rh_daily = daily_profile(df, "RH", global_local)
+    return dbt_daily
 
+
+@app.callback(
+    Output("heatmap-dbt", "figure"),
+    [Input("global-local-radio-input", "value")],
+    [State("df-store", "data")],
+)
+@code_timer
+@cache.memoize(timeout=TIMEOUT)
+def update_tab_three_db_heatmap(global_local, df):
+    """Update the contents of tab three. Passing in general info (df, meta)."""
+    df = pd.read_json(df, orient="split")
     # Heatmap Graphs
     dbt_heatmap = heatmap(df, "DBT", global_local)
+    return dbt_heatmap
+
+
+@app.callback(
+    Output("yearly-rh", "figure"),
+    [Input("global-local-radio-input", "value")],
+    [State("df-store", "data")],
+)
+@code_timer
+def update_tab_three_rh_yearly(global_local, df):
+    """Update the contents of tab three. Passing in general info (df, meta)."""
+    df = pd.read_json(df, orient="split")
+    rh_yearly = yearly_profile(df, "RH", global_local)
+    rh_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
+
+    return rh_yearly
+
+
+@app.callback(
+    Output("daily-rh", "figure"),
+    [Input("global-local-radio-input", "value")],
+    [State("df-store", "data")],
+)
+@code_timer
+@cache.memoize(timeout=TIMEOUT)
+def update_tab_three_rh_daily(global_local, df):
+    """Update the contents of tab three. Passing in general info (df, meta)."""
+    df = pd.read_json(df, orient="split")
+    # Daily Profile Graphs
+    rh_daily = daily_profile(df, "RH", global_local)
+
+    return rh_daily
+
+
+@app.callback(
+    Output("heatmap-rh", "figure"),
+    [Input("global-local-radio-input", "value")],
+    [State("df-store", "data")],
+)
+@code_timer
+@cache.memoize(timeout=TIMEOUT)
+def update_tab_three_rh_heatmap(global_local, df):
+    """Update the contents of tab three. Passing in general info (df, meta)."""
+    df = pd.read_json(df, orient="split")
+    # Heatmap Graphs
     rh_heatmap = heatmap(df, "RH", global_local)
 
-    return dbt_yearly, dbt_daily, dbt_heatmap, rh_yearly, rh_daily, rh_heatmap
+    return rh_heatmap
 
 
 #####################
