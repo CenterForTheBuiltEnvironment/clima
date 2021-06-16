b48e32bc9d68f8fdb6998ea8c4634329376e2da9
FedericoTartarini <federicotartarini@gmail.com>
2021-06-16 17:23:22 +0800

    feat: t_rh tab added dropdown to choose variable

--- my_project/tab_t_rh/app_t_rh.py
+++ my_project/tab_t_rh/app_t_rh.py
@@ -1,7 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from my_project.utils import generate_chart_name
+from my_project.utils import generate_chart_name, code_timer
 from my_project.template_graphs import heatmap, yearly_profile, daily_profile
 import pandas as pd
 
@@ -10,136 +10,125 @@ from app import app, cache, TIMEOUT
 
 def layout_t_rh():
     return html.Div(
-        className="container-col",
+        className="container-col full-width",
         children=[
-            html.H5("Dry Bulb Temperature"),
-            dcc.Loading(
-                type="circle",
-                children=[html.Div(id="yearly-dbt")],
+            html.Div(
+                className="container-row full-width text-dropdown-container",
+                children=[
+                    html.H6(className="text-next-to-input", children=["Variable: "]),
+                    dcc.Dropdown(
+                        id="dropdown",
+                        className="dropdown-t-rh",
+                        options=[
+                            {"label": "Dry Bulb Temperature", "value": "dd_tdb"},
+                            {"label": "Relative Humidity", "value": "dd_rh"},
+                        ],
+                        value="dd_tdb",
+                    ),
+                ],
             ),
-            dcc.Loading(
-                type="circle",
-                children=[html.Div(id="daily-dbt")],
-            ),
-            dcc.Loading(
-                type="circle",
-                children=[html.Div(id="heatmap-dbt")],
-            ),
-            html.H5("Relative Humidity"),
-            dcc.Loading(
-                type="circle",
-                children=[html.Div(id="yearly-rh")],
-            ),
-            dcc.Loading(
-                type="circle",
-                children=[html.Div(id="daily-rh")],
-            ),
-            dcc.Loading(
-                type="circle",
-                children=[html.Div(id="heatmap-rh")],
+            html.Div(
+                className="container-col",
+                children=[
+                    dcc.Loading(
+                        id="loading-1",
+                        type="circle",
+                        children=html.Div(id="yearly"),
+                        style={"height": "20px"},
+                    ),
+                    dcc.Loading(
+                        id="loading-2",
+                        type="circle",
+                        children=html.Div(id="daily"),
+                        style={"height": "20px"},
+                    ),
+                    dcc.Loading(
+                        id="loading-3",
+                        type="circle",
+                        children=html.Div(id="heatmap"),
+                        style={"height": "20px"},
+                    ),
+                ],
             ),
         ],
     )
 
 
 @app.callback(
-    Output("yearly-dbt", "children"),
+    Output("yearly", "children"),
     [Input("global-local-radio-input", "value")],
+    [Input("dropdown", "value")],
     [State("df-store", "data")],
     [State("meta-store", "data")],
 )
 @cache.memoize(timeout=TIMEOUT)
-def update_tab_three_db_yearly(global_local, df, meta):
+@code_timer
+def update_yearly(global_local, dd_value, df, meta):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
-    # Yearly Graphs
-    dbt_yearly = yearly_profile(df, "DBT", global_local)
-    dbt_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
 
-    return dcc.Graph(
-        config=generate_chart_name("tmp_rh", meta),
-        figure=dbt_yearly,
-    )
+    if dd_value == "dd_tdb":
+        dbt_yearly = yearly_profile(df, "DBT", global_local)
+        dbt_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
+
+        return dcc.Graph(
+            config=generate_chart_name("tmp_rh", meta),
+            figure=dbt_yearly,
+        )
+    else:
+        rh_yearly = yearly_profile(df, "RH", global_local)
+        rh_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
+
+        return dcc.Graph(
+            config=generate_chart_name("tmp_rh", meta),
+            figure=rh_yearly,
+        )
 
 
 @app.callback(
-    Output("daily-dbt", "children"),
+    Output("daily", "children"),
     [Input("global-local-radio-input", "value")],
+    [Input("dropdown", "value")],
     [State("df-store", "data")],
     [State("meta-store", "data")],
 )
 @cache.memoize(timeout=TIMEOUT)
-def update_tab_three_db_daily(global_local, df, meta):
+@code_timer
+def update_tab_three_db_daily(global_local, dd_value, df, meta):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
-    return dcc.Graph(
-        config=generate_chart_name("tmp_rh", meta),
-        figure=daily_profile(df, "DBT", global_local),
-    )
+
+    if dd_value == "dd_tdb":
+        return dcc.Graph(
+            config=generate_chart_name("tmp_rh", meta),
+            figure=daily_profile(df, "DBT", global_local),
+        )
+    else:
+        return dcc.Graph(
+            config=generate_chart_name("tmp_rh", meta),
+            figure=daily_profile(df, "RH", global_local),
+        )
 
 
 @app.callback(
-    Output("heatmap-dbt", "children"),
+    Output("heatmap", "children"),
     [Input("global-local-radio-input", "value")],
+    [Input("dropdown", "value")],
     [State("df-store", "data")],
     [State("meta-store", "data")],
 )
 @cache.memoize(timeout=TIMEOUT)
-def update_tab_three_db_heatmap(global_local, df, meta):
+@code_timer
+def update_tab_three_db_heatmap(global_local, dd_value, df, meta):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
-    return dcc.Graph(
-        config=generate_chart_name("tmp_rh", meta),
-        figure=heatmap(df, "DBT", global_local),
-    )
-
-
-@app.callback(
-    Output("yearly-rh", "children"),
-    [Input("global-local-radio-input", "value")],
-    [State("df-store", "data")],
-    [State("meta-store", "data")],
-)
-@cache.memoize(timeout=TIMEOUT)
-def update_tab_three_rh_yearly(global_local, df, meta):
-    """Update the contents of tab three. Passing in general info (df, meta)."""
-    df = pd.read_json(df, orient="split")
-    rh_yearly = yearly_profile(df, "RH", global_local)
-    rh_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
-
-    return dcc.Graph(
-        config=generate_chart_name("tmp_rh", meta),
-        figure=rh_yearly,
-    )
-
-
-@app.callback(
-    Output("daily-rh", "children"),
-    [Input("global-local-radio-input", "value")],
-    [State("df-store", "data")],
-    [State("meta-store", "data")],
-)
-@cache.memoize(timeout=TIMEOUT)
-def update_tab_three_rh_daily(global_local, df, meta):
-    """Update the contents of tab three. Passing in general info (df, meta)."""
-    df = pd.read_json(df, orient="split")
-    return dcc.Graph(
-        config=generate_chart_name("tmp_rh", meta),
-        figure=daily_profile(df, "RH", global_local),
-    )
-
-
-@app.callback(
-    Output("heatmap-rh", "children"),
-    [Input("global-local-radio-input", "value")],
-    [State("df-store", "data")],
-    [State("meta-store", "data")],
-)
-@cache.memoize(timeout=TIMEOUT)
-def update_tab_three_rh_heatmap(global_local, df, meta):
-    """Update the contents of tab three. Passing in general info (df, meta)."""
-    df = pd.read_json(df, orient="split")
-    return dcc.Graph(
-        config=generate_chart_name("tmp_rh", meta),
-        figure=heatmap(df, "RH", global_local),
-    )
+    if dd_value == "dd_tdb":
+        return dcc.Graph(
+            config=generate_chart_name("tmp_rh", meta),
+            figure=heatmap(df, "DBT", global_local),
+        )
+    else:
+        return dcc.Graph(
+            config=generate_chart_name("tmp_rh", meta),
+            figure=heatmap(df, "RH", global_local),
+        )
