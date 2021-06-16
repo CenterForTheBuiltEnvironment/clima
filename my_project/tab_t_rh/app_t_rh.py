850e710cb109ccf883b2b73894b91e957284a1fb
FedericoTartarini <federicotartarini@gmail.com>
2021-06-16 15:29:27 +0800

    feat: t_rh tab changed name charts when downloaded

--- my_project/tab_t_rh/app_t_rh.py
+++ my_project/tab_t_rh/app_t_rh.py
@@ -1,6 +1,5 @@
 import dash_core_components as dcc
 import dash_html_components as html
-from my_project.global_scheme import fig_config
 from dash.dependencies import Input, Output, State
 from my_project.utils import generate_chart_name
 from my_project.template_graphs import heatmap, yearly_profile, daily_profile
@@ -20,40 +19,29 @@ def layout_t_rh():
             ),
             dcc.Loading(
                 type="circle",
-                children=[
-                    dcc.Graph(id="daily-dbt", config=fig_config),
-                ],
+                children=[html.Div(id="daily-dbt")],
             ),
             dcc.Loading(
                 type="circle",
-                children=[
-                    dcc.Graph(id="heatmap-dbt", config=fig_config),
-                ],
+                children=[html.Div(id="heatmap-dbt")],
             ),
             html.H5("Relative Humidity"),
             dcc.Loading(
                 type="circle",
-                children=[
-                    dcc.Graph(id="yearly-rh", config=fig_config),
-                ],
+                children=[html.Div(id="yearly-rh")],
             ),
             dcc.Loading(
                 type="circle",
-                children=[
-                    dcc.Graph(id="daily-rh", config=fig_config),
-                ],
+                children=[html.Div(id="daily-rh")],
             ),
             dcc.Loading(
                 type="circle",
-                children=[
-                    dcc.Graph(id="heatmap-rh", config=fig_config),
-                ],
+                children=[html.Div(id="heatmap-rh")],
             ),
         ],
     )
 
 
-# TAB THREE: TEMP AND HUMIDITY
 @app.callback(
     Output("yearly-dbt", "children"),
     [Input("global-local-radio-input", "value")],
@@ -69,78 +57,89 @@ def update_tab_three_db_yearly(global_local, df, meta):
     dbt_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
 
     return dcc.Graph(
-        config=generate_chart_name("t_rh", meta),
+        config=generate_chart_name("tmp_rh", meta),
         figure=dbt_yearly,
     )
 
 
 @app.callback(
-    Output("daily-dbt", "figure"),
+    Output("daily-dbt", "children"),
     [Input("global-local-radio-input", "value")],
     [State("df-store", "data")],
+    [State("meta-store", "data")],
 )
 @cache.memoize(timeout=TIMEOUT)
-def update_tab_three_db_daily(global_local, df):
+def update_tab_three_db_daily(global_local, df, meta):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
-    dbt_daily = daily_profile(df, "DBT", global_local)
-    return dbt_daily
+    return dcc.Graph(
+        config=generate_chart_name("tmp_rh", meta),
+        figure=daily_profile(df, "DBT", global_local),
+    )
 
 
 @app.callback(
-    Output("heatmap-dbt", "figure"),
+    Output("heatmap-dbt", "children"),
     [Input("global-local-radio-input", "value")],
     [State("df-store", "data")],
+    [State("meta-store", "data")],
 )
 @cache.memoize(timeout=TIMEOUT)
-def update_tab_three_db_heatmap(global_local, df):
+def update_tab_three_db_heatmap(global_local, df, meta):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
-    # Heatmap Graphs
-    dbt_heatmap = heatmap(df, "DBT", global_local)
-    return dbt_heatmap
+    return dcc.Graph(
+        config=generate_chart_name("tmp_rh", meta),
+        figure=heatmap(df, "DBT", global_local),
+    )
 
 
 @app.callback(
-    Output("yearly-rh", "figure"),
+    Output("yearly-rh", "children"),
     [Input("global-local-radio-input", "value")],
     [State("df-store", "data")],
+    [State("meta-store", "data")],
 )
 @cache.memoize(timeout=TIMEOUT)
-def update_tab_three_rh_yearly(global_local, df):
+def update_tab_three_rh_yearly(global_local, df, meta):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
     rh_yearly = yearly_profile(df, "RH", global_local)
     rh_yearly.update_layout(xaxis=dict(rangeslider=dict(visible=True)))
 
-    return rh_yearly
+    return dcc.Graph(
+        config=generate_chart_name("tmp_rh", meta),
+        figure=rh_yearly,
+    )
 
 
 @app.callback(
-    Output("daily-rh", "figure"),
+    Output("daily-rh", "children"),
     [Input("global-local-radio-input", "value")],
     [State("df-store", "data")],
+    [State("meta-store", "data")],
 )
 @cache.memoize(timeout=TIMEOUT)
-def update_tab_three_rh_daily(global_local, df):
+def update_tab_three_rh_daily(global_local, df, meta):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
-    # Daily Profile Graphs
-    rh_daily = daily_profile(df, "RH", global_local)
-
-    return rh_daily
+    return dcc.Graph(
+        config=generate_chart_name("tmp_rh", meta),
+        figure=daily_profile(df, "RH", global_local),
+    )
 
 
 @app.callback(
-    Output("heatmap-rh", "figure"),
+    Output("heatmap-rh", "children"),
     [Input("global-local-radio-input", "value")],
     [State("df-store", "data")],
+    [State("meta-store", "data")],
 )
 @cache.memoize(timeout=TIMEOUT)
-def update_tab_three_rh_heatmap(global_local, df):
+def update_tab_three_rh_heatmap(global_local, df, meta):
     """Update the contents of tab three. Passing in general info (df, meta)."""
     df = pd.read_json(df, orient="split")
-    # Heatmap Graphs
-    rh_heatmap = heatmap(df, "RH", global_local)
-
-    return rh_heatmap
+    return dcc.Graph(
+        config=generate_chart_name("tmp_rh", meta),
+        figure=heatmap(df, "RH", global_local),
+    )
