b0661781553d67580e79b60aafa2a0c175bf7386
FedericoTartarini <federicotartarini@gmail.com>
2021-06-22 14:17:55 +0800

    feat: improved look of upload button, set max height for current location and alert, removed display epw when uploaded, handling alert message inside submit_button

--- my_project/tab_select/app_select.py
+++ my_project/tab_select/app_select.py
@@ -34,31 +34,22 @@ def layout_select():
                     ),
                 ],
             ),
-            html.Div(
-                children=[
-                    dcc.Upload(
-                        id="upload-data",
-                        children=html.Div(
-                            [
-                                "Drag and Drop or ",
-                                html.A("Select Files"),
-                                " your EPW file",
-                            ]
-                        ),
-                        style={
-                            "width": "98%",
-                            "height": "50px",
-                            "lineHeight": "60px",
-                            "borderWidth": "1px",
-                            "borderStyle": "dashed",
-                            "borderRadius": "5px",
-                            "textAlign": "center",
-                            "margin": "10px",
-                        },
-                        # Allow multiple files to be uploaded
-                        multiple=True,
-                    ),
-                ],
+            dcc.Upload(
+                id="upload-data",
+                children=dbc.Button(
+                    [
+                        "Drag and Drop or ",
+                        html.A("Select Files"),
+                        " your EPW file",
+                    ],
+                    outline=True,
+                    color="secondary",
+                    className="mt-2",
+                    block=True,
+                    style={"borderRadius": "5px", "borderStyle": "dashed"},
+                ),
+                # Allow multiple files to be uploaded
+                multiple=True,
             ),
             html.Div(id="output-data-upload"),
             html.Embed(id="tab-one-map", src="https://www.ladybug.tools/epwmap/"),
@@ -79,9 +70,12 @@ def alert():
     return html.Div(
         [
             dbc.Alert(
+                "Submit a link below or upload an EPW file!",
+                color="primary",
                 id="alert",
                 dismissable=True,
-                is_open=False,
+                is_open=True,
+                style={"maxHeight": "66px"},
             )
         ]
     )
@@ -91,17 +85,21 @@ def alert():
 @app.callback(
     Output("df-store", "data"),
     Output("meta-store", "data"),
+    Output("alert", "is_open"),
+    Output("alert", "children"),
+    Output("alert", "color"),
+    Output("banner-subtitle", "children"),
     Input("submit-button", "n_clicks"),
     Input("upload-data", "contents"),
     State("upload-data", "filename"),
-    State("upload-data", "last_modified"),
     State("input-url", "value"),
     prevent_initial_call=True,
 )
 @code_timer
 # @cache.memoize(timeout=TIMEOUT)
-def submit_button(n_clicks, list_of_contents, list_of_names, list_of_dates, url):
+def submit_button(n_clicks, list_of_contents, list_of_names, url):
     """Takes the input once submitted and stores it."""
+    default = "Current Location: N/A"
 
     ctx = dash.callback_context
     print(ctx.triggered[0]["prop_id"])
@@ -112,18 +110,28 @@ def submit_button(n_clicks, list_of_contents, list_of_names, list_of_dates, url)
         else:
             lines = get_data(url)
             if lines is None:
-                return None, None
+                return (
+                    None,
+                    None,
+                    True,
+                    "This link you have selected is not available. Please choose another one.",
+                    "warning",
+                    default,
+                )
             df, meta = create_df(lines, url)
-            if df is not None:
-                # fixme: DeprecationWarning: an integer is required (got type float).
-                df = df.to_json(date_format="iso", orient="split")
-                # todo I should update the input value with the last entered
-                return df, meta
-            else:
-                return None, None
+            # fixme: DeprecationWarning: an integer is required (got type float).
+            df = df.to_json(date_format="iso", orient="split")
+            # todo I should update the input value with the last entered
+            return (
+                df,
+                meta,
+                True,
+                "Successfully loaded data. You can change location by submitting a link below or by uploading your EPW file!",
+                "success",
+                "Current Location: " + meta[1] + ", " + meta[3],
+            )
 
     else:
-        print(list_of_contents, list_of_names, list_of_dates)
         if list_of_contents is not None:
             content_type, content_string = list_of_contents[0].split(",")
 
@@ -132,46 +140,35 @@ def submit_button(n_clicks, list_of_contents, list_of_names, list_of_dates, url)
                 if "epw" in list_of_names[0]:
                     # Assume that the user uploaded a CSV file
                     lines = io.StringIO(decoded.decode("utf-8")).read().split("\n")
-                    df, meta_data = create_df(lines, list_of_names[0])
+                    df, meta = create_df(lines, list_of_names[0])
                     df = df.to_json(date_format="iso", orient="split")
-                    return df, meta_data
+                    return (
+                        df,
+                        meta,
+                        True,
+                        "Successfully loaded your EPW file. You can change location by submitting a link below or by uploading your EPW file!",
+                        "success",
+                        "Current Location: " + meta[1] + ", " + meta[3],
+                    )
                 else:
-                    return None, None
+                    return (
+                        None,
+                        None,
+                        True,
+                        "The format of the EPW file you have uploaded is invalid.",
+                        "warning",
+                        default,
+                    )
             except Exception as e:
                 print(e)
-                return None, None
-
-
-@app.callback(
-    Output("alert", "is_open"),
-    Output("alert", "children"),
-    Output("alert", "color"),
-    Output("banner-subtitle", "children"),
-    [Input("df-store", "data")],
-    [Input("submit-button", "n_clicks")],
-    [State("meta-store", "data")],
-)
-def alert_display(data, n_clicks, meta):
-    """Displays the alert for the submit button."""
-    default = "Current Location: N/A"
-    # todo store click count in memory
-    if data is None and n_clicks is None:
-        return True, "To start, submit a link below!", "primary", default
-    elif data is None and n_clicks > 0:
-        return (
-            True,
-            "This link is not available. Please choose another one.",
-            "warning",
-            default,
-        )
-    else:
-        subtitle = "Current Location: " + meta[1] + ", " + meta[3]
-        return (
-            True,
-            "Successfully loaded data. You can change location by submitting a link below or by uploading your EPW file!",
-            "success",
-            subtitle,
-        )
+                return (
+                    None,
+                    None,
+                    True,
+                    "The file you have uploaded is not an EPW file",
+                    "warning",
+                    default,
+                )
 
 
 @app.callback(
@@ -211,51 +208,3 @@ def on_data(ts, meta):
         default_url = meta[-1]
 
     return default_url
-
-
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(",")
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if "epw" in filename:
-            # Assume that the user uploaded a CSV file
-            lines = io.StringIO(decoded.decode("utf-8")).read().split("\n")
-            df, meta_data = create_df(lines, filename)
-            df = df.head()
-    except Exception as e:
-        print(e)
-        return html.Div(["There was an error processing this file."])
-
-    return html.Div(
-        [
-            html.H5(filename),
-            html.H6(datetime.datetime.fromtimestamp(date)),
-            dash_table.DataTable(
-                data=df.to_dict("records"),
-                columns=[{"name": i, "id": i} for i in df.columns],
-            ),
-            html.Hr(),  # horizontal line
-            # For debugging, display the raw contents provided by the web browser
-            html.Div("Raw Content"),
-            html.Pre(
-                contents[0:200] + "...",
-                style={"whiteSpace": "pre-wrap", "wordBreak": "break-all"},
-            ),
-        ]
-    )
-
-
-@app.callback(
-    Output("output-data-upload", "children"),
-    Input("upload-data", "contents"),
-    State("upload-data", "filename"),
-    State("upload-data", "last_modified"),
-)
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d)
-            for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
-        ]
-        return children
