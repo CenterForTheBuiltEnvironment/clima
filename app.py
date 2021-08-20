3812e40f494740f79b3c98647cadae4da6054640
FedericoTartarini <federicotartarini@gmail.com>
2021-08-20 18:16:18 +0800

    feat: added google tag

--- app.py
+++ app.py
@@ -20,3 +20,31 @@ cache = Cache(
 )
 TIMEOUT = 600
 app.config.suppress_callback_exceptions = True
+
+app.index_string = """<!DOCTYPE html>
+<html>
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3VD48BMBN2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3VD48BMBN2');
+</script>
+{%metas%}
+<title>{%title%}</title>
+{%favicon%}
+{%css%}
+</head>
+<body>
+{%app_entry%}
+<footer>
+{%config%}
+{%scripts%}
+{%renderer%}
+</footer>
+</body>
+</html>
+"""
