4da22646c6964018097cf7ed112c42c11d3c86a8
FedericoTartarini <federicotartarini@gmail.com>
2021-08-23 13:44:41 +0800

    Bump version: 0.4.3 → 0.4.4

--- my_project/layout.py
+++ my_project/layout.py
@@ -27,7 +27,7 @@ def footer():
                         href="https://creativecommons.org/licenses/by/4.0/",
                     ),
                     html.A(
-                        "Version: 0.4.3",
+                        "Version: 0.4.4",
                         href="/changelog",
                     ),
                 ],
