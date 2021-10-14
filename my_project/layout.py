b3bec2839aed4a3766dd31d9369817073ac465cf
FedericoTartarini <federicotartarini@gmail.com>
2021-10-14 11:17:31 +0800

    Bump version: 0.4.6 → 0.5.0

--- my_project/layout.py
+++ my_project/layout.py
@@ -28,7 +28,7 @@ def footer():
                     ),
                     dbc.Row(
                         html.A(
-                            "Version: 0.4.6",
+                            "Version: 0.5.0",
                             href="https://center-for-the-built-environment.gitbook.io/clima/version/changelog",
                         ),
                     ),
