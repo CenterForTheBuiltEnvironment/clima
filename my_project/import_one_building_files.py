7d9d3cbf4be6ba969cd112c6c03591b244469085
Federico <federicotartarini@gmail.com>
2022-04-14 12:04:45 +0800

    feat: added new EPW files from oneBuilding.org

--- my_project/import_one_building_files.py
+++ my_project/import_one_building_files.py
@@ -5,7 +5,7 @@ import re
 
 @code_timer
 def import_kml_files(file_name):
-    with open(f"./assets/data/{file_name}.kml", encoding="utf8") as data:
+    with open(f"./assets/data/{file_name}", encoding="utf8", errors="ignore") as data:
         text = data.read()
 
     locations = re.findall(r"<Placemark>[\s\S]*?<\/Placemark>", text)
@@ -37,7 +37,7 @@ def import_kml_files(file_name):
 
     try:
         df_old = pd.read_csv(f"./assets/data/one_building.csv")
-        df_old = df_old.append(df).drop_duplicates()
+        df_old = pd.concat([df_old, df]).drop_duplicates()
         df_old.to_csv(f"./assets/data/one_building.csv", index=False)
     except FileNotFoundError:
         df.to_csv(f"./assets/data/one_building.csv", index=False)
@@ -45,14 +45,11 @@ def import_kml_files(file_name):
 
 if __name__ == "__main__":
 
-    import_kml_files("Region1_Africa_EPW_Processing_locations")
-    import_kml_files("Region7_Antarctica_EPW_Processing_locations")
-    import_kml_files("Region3_South_America_EPW_Processing_locations")
-    import_kml_files("Region2_Region6_Russia_EPW_Processing_locations")
-    import_kml_files("Region2_Asia_EPW_Processing_locations")
-    import_kml_files("Region4_CaliforniaClimateZones_EPW_Processing_locations")
-    import_kml_files("Region4_NA_CA_Caribbean_EPW_Processing_locations")
-    import_kml_files("Region4_USA_EPW_Processing_locations")
-    import_kml_files("Region4_Canada_EPW_Processing_locations")
-    import_kml_files("Region6_Europe_EPW_Processing_locations")
-    import_kml_files("Region5_Southwest_Pacific_EPW_Processing_locations")
+    import os
+
+    directory = os.fsencode("./assets/data/")
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".kml"):
+            import_kml_files(filename)
