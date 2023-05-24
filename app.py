715772fce9bd6b5e8ffc5b1b13f4529334046a10
federico Tartarini <federicotartarini@gmail.com>
2023-05-24 15:22:23 +1000

    build: added Google Tag manager

--- app.py
+++ app.py
@@ -29,6 +29,13 @@ app.index_string = """<!DOCTYPE html>
     
       gtag('config', 'G-JDQTBEPS4B');
     </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WTDGD6Q');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="co-authored by Giovanni Betti, Federico Tartarini, Christine Nguyen, Yunzhu Ji">
@@ -42,6 +49,10 @@ app.index_string = """<!DOCTYPE html>
     {%css%}
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTDGD6Q"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 {%app_entry%}
 <footer>
 {%config%}
