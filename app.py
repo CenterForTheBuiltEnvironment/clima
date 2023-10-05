028154a5e3b7161fbbd6bfbe31f9973bd0bceffe
federico Tartarini <federicotartarini@gmail.com>
2023-10-05 17:41:59 +1100

    build: made the application installable

--- app.py
+++ app.py
@@ -42,6 +42,8 @@ app.index_string = """<!DOCTYPE html>
     <meta name="keywords" content="Climate analysis, EPW, Weather files, Data visualization, Building design">
     <meta name="description" content="The CBE Clima Tool is a web-based application built to support climate analysis specifically designed to support the need of architects and engineers interested in climate-adapted design. It allows users to analyze the climate data of more than 27,500 locations worldwide from both Energy Plus and Climate.One.Building.org. You can, however, also choose to upload your own EPW weather file. Our tool can be used to analyze and visualize data contained in EnergyPlus Weather (EPW) files. It furthermore calculates a number of climate-related values (i.e. solar azimuth and altitude, Universal Thermal Climate Index (UTCI), comfort indices, etc.) that are not contained in the EPW files but can be derived from the information therein contained. It can be freely accessed at clima.cbe.berkeley.edu">
     <title>CBE Clima Tool</title>
+    <link rel="manifest" href="./assets/manifest.json">
+    <meta name="theme-color" content="#003262" />
     <meta property="og:image" content="https://github.com/CenterForTheBuiltEnvironment/clima/tree/main/assets/og-image.png">
     <meta property="og:description" content="The CBE Clima Tool is a web-based application built to support climate analysis specifically designed to support the need of architects and engineers interested in climate-adapted design.">
     <meta property="og:title" content="CBE Clima Tool">
