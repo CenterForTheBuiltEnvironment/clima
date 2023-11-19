da611e0c15f390b679955eecf4fe7b705b14bf0b
mccalluc <mccalluc@users.noreply.github.com>
2023-11-18 22:37:10 -0500

    minimal test of banner content

--- tests/node/cypress/e2e/spec.cy.js
+++ tests/node/cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://127.0.0.1:8080');
+    cy.contains('CBE Clima Tool');
+    cy.contains('Current Location: N/A');
   })
 })
