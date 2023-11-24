baeb8fa4cfe6812a8ede87eb43f36d1b530d7f8f
mccalluc <mccalluc@users.noreply.github.com>
2023-11-24 08:33:18 -0500

    add a tab click to cypress test

--- tests/node/cypress/e2e/spec.cy.js
+++ tests/node/cypress/e2e/spec.cy.js
@@ -3,5 +3,12 @@ describe('template spec', () => {
     cy.visit('http://127.0.0.1:8080');
     cy.contains('CBE Clima Tool');
     cy.contains('Current Location: N/A');
+    
+    // Upload
+    cy.get('input[type=file]').selectFile('test.epw', {force: true});
+    cy.contains('The EPW was successfully loaded!');
+
+    cy.get('.custom-tab').not('.tab--disabled').contains('Climate Summary', {timeout: 30 * 1000}).click();
+    cy.contains('data collected between 2004 and 2018')
   })
 })
