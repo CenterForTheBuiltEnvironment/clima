79db946c263aeb78b0495b930e7f07598d821231
yluo0664 <154036738+LeoLuosifen@users.noreply.github.com>
2025-08-23 14:28:32 +1000

    feat: add Global Column Names class to replace string

---
+++ pages/lib/global_column_names.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+class ColNames(str, Enum):
+    # ==================== Time related column ====================
+    YEAR = "year"           # year
+    MONTH = "month"         # month
+    DAY = "day"             # day
+    HOUR = "hour"           # hour
+    MINUTE = "minute"       # minute
+    
+    # ==================== Meteorological data column ====================
+    DBT = "DBT"             # Dry Bulb Temperature
+    DPT = "DPT"             # Dew Point Temperature
+    RH = "RH"               # Relative Humidity
+    P_ATM = "p_atm"         # Atmospheric Pressure
+    
+    # ==================== Radiation-related column ====================
+    EXTR_HOR_RAD = "extr_hor_rad"      # Extraterrestrial Horizontal Radiation
+    HOR_IR_RAD = "hor_ir_rad"          # Horizontal Infrared Radiation
+    GLOB_HOR_RAD = "glob_hor_rad"      # Global Horizontal Radiation
+    DIR_NOR_RAD = "dir_nor_rad"        # Direct Normal Radiation
+    DIF_HOR_RAD = "dif_hor_rad"        # Diffuse Horizontal Radiation
+    
+    # ==================== Lighting-related columns ====================
+    GLOB_HOR_ILL = "glob_hor_ill"      # Global Horizontal Illuminance
+    DIR_NOR_ILL = "dir_nor_ill"        # Direct Normal Illuminance
+    DIF_HOR_ILL = "dif_hor_ill"        # Diffuse Horizontal Illuminance
+    
+    # ==================== Other ====================
+    ZLUMI = "Zlumi"                    # Luminance
+    WIND_DIR = "wind_dir"              # Wind Direction
+    WIND_SPEED = "wind_speed"          # Wind Speed
+    TOT_SKY_COVER = "tot_sky_cover"    # Total Sky Cover
+    OSKYCOVER = "Oskycover"            # Opaque Sky Cover
+    VIS = "Vis"                        # Visibility
+    CHEIGHT = "Cheight"                # Cloud Height
+    PWobs = "PWobs"                    # Precipitation Observation
+    PWcodes = "PWcodes"                # Precipitation Codes
+    Pwater = "Pwater"                  # Precipitation Water
+    AsolOptD = "AsolOptD"              # Aerosol Optical Depth
+    SnowD = "SnowD"                    # Snow Depth
+    DaySSnow = "DaySSnow"              # Daily Snow
+    
+    # ==================== Calculation column ====================
+    FAKE_YEAR = "fake_year"            # Fake Year
+    MONTH_NAMES = "month_names"        # Month names
+    UTC_TIME = "UTC_time"              # UTC Time
+    DOY = "DOY"                        # Day of Year
